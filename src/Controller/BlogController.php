6c3a7101de249ee40b6e40270cefe6c1a776acd7
Fourat Ben Dhafer <fouratbendhafer99@gmail.com>
2022-02-16 17:37:19 +0100

    Blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,14 +7,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
+    /**
+     * @Route("/blogs")
+     */
 {
     /**
-     * @Route("/blogs", name="blog")
+     * @Route("/", name="blogs")
      */
     public function index(): Response
     {
         return $this->render('blogs/index.html.twig', [
             'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blogs_viewBlog")
+     */
+    public function viewBlog($id): Response
+    {
+        return $this->render('blogs/blog.html.twig',[
+
         ]);
     }
 }
